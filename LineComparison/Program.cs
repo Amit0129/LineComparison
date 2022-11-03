a3f2d0917a0f3b5d0d05fda1bbd41e743ca80281
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-03 15:38:52 +0530

    UC1:-Using Cartesian System To Calculate Line Length

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("======Welcome to Line Comparison Computation======");
+
+
+            Console.Write("Enter the x1 of the Cordinate (x1,y1): ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the y1 of the Cordinate (x1,y1): ");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the x2 of the Cordinate (x1,y1): ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the y3 of the Cordinate (x1,y1): ");
+            double y2 = double.Parse(Console.ReadLine());
+
+            double temp =Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+            Console.WriteLine(temp);
+
         }
     }
 }
